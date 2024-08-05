e94fd10eefcee87d8f5d0fd43443fb65009502f6
pamod <pamod@wso2.com>
2024-08-05 11:02:26 +0530

    Add dashboard k8s scripts

--- banking/undeploy.sh
+++ banking/undeploy.sh
@@ -5,6 +5,7 @@ kubectl delete service wso2am-service
 kubectl delete service wso2iam-service
 kubectl delete service wso2mi-service
 kubectl delete service paymentsvc
+kubectl delete service wso2mi-dashboard-svc
 
 #Remove all deployments 
 
@@ -13,6 +14,7 @@ kubectl delete deployment wso2am-deployment
 kubectl delete deployment wso2iam-deployment
 kubectl delete deployment wso2mi-deployment
 kubectl delete deployment paymentsvc
+kubectl delete deployment wso2mi-dashboard-deployment
 
 #Remove all images 
 
@@ -21,6 +23,7 @@ docker rmi -f wso2.demo/wso2apim:v4.3.0
 docker rmi -f wso2.demo/wso2iam:v7.0.0
 docker rmi -f wso2.demo/wso2mi:v4.2.0
 docker rmi -f wso2.demo/paymentsvc:v1
+docker rmi -f wso2.demo/wso2mi-dashboard
 
 #Remove all orphen images
 
